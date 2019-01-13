0d240f54e31d08c3cf42dff9a9603e4bcfa97edc
mtrusca24 <truscamarius@gmail.com>
2019-01-13 23:28:15 +0200

    primul commit

---
+++ src/facBranchsiPushtoGitHub/Cefaci.java
@@ -0,0 +1,15 @@
+package facBranchsiPushtoGitHub;
+
+public class Cefaci {
+
+	public static void main(String[] args) {
+
+ System.out.println("ce faci fatoooo");
+ 
+ /*
+  * 1. git init la folderul GitandgitHub 
+  */
+
+	}
+
+}
